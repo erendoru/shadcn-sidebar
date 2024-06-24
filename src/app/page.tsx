f803a83cc596ffe680c4d5fc9a10739746cb7de5
erendoru <erendoru@hotmail.com>
2024-06-25 02:45:46 +0300

    dark mode added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ export default function Dashboard() {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <Sidebar />
-      <MobileNav />
+      <div className="p-5">
+        <MobileNav />
+      </div>
     </div>
   );
 }
